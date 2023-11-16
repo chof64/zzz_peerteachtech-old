6fc9d0c95894bed4dec43a867e97c2867b6fb949
Chad Fernandez <contact@chadfernandez.me>
2023-11-16 15:19:07 +0800

    Adding tailwind css integration (#1)
    * adding tailwindcss integration to astro
    
    * adding custom page and tailwind container style
    
    * removed default card

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans, "sans-serif"],
+        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono, "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
