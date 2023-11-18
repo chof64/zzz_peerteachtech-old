0d11437d0471b1d6db48c562c3998f47778192ef
Chad Fernandez <contact@chadfernandez.me>
2023-11-18 10:22:25 +0800

    moved to vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
-
 import partytown from "@astrojs/partytown";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +15,5 @@ export default defineConfig({
     }),
   ],
   output: "server",
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
